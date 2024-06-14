7d0768d9e5f6fb117da5fe7f48be7a36daf1da43
Pratik Mali <malipratik999@gmail.com>
2024-06-14 11:02:23 +0530

    Update api.js
    evn files token update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTIxYjgxYWU2MDEyNWM0Nzg3NDc1MGFkMjBjNDlhMSIsInN1YiI6IjY2NWVhMjRiYjhkMjEzOTE5YjFjMzAwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UkrorSgC3vVtEx0J1Piqn7y5l381Lv4m-92fYrAd83k;
 
 const headers = {
     Authorization: "bearer " + TMDB_TOKEN,
